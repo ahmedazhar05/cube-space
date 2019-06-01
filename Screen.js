c96b533fdbc84c5b87ed0155cd157fab0805fd3a
Azhar Ahmed <49714602+ahmedazhar05@users.noreply.github.com>
2019-06-02 00:11:23 +0530

    Screen Fix
    Screen (not 600) compatible

--- Screen.js
+++ Screen.js
@@ -4,19 +4,19 @@ class Screen {
   }
   start() {
     push();
-    translate(0, 0, 490);
+    translate(0, 0, width*49/60);
     this.format(getImage(1), getBase().w, getBase().h / 3);
     pop();
   }
   enter() {
     let inc = 3;
     push();
-    translate(mybox.x, -180, mybox.y-400);
+    translate(mybox.x, /*-180*/-height/2 + (height/15)*inc, /*mybox.y-400*/0);
     noStroke();
     texture(getImage(5));
     plane((width/10)*inc, (height/15)*inc);
     //translate(mybox.x, mybox.z + getSize(true)/2, mybox.y);
-    translate(0, mybox.z + getSize(true)/2 +180, 400);
+    translate(0, +height/2 - (height/15)*inc + mybox.z + getSize(true)/2/*mybox.z + getSize(true)/2 +180*/, mybox.y);
     rotateX(millis()/10);
     translate(getSize(true)*3/2, 0,0);
     this.arrow();
@@ -33,7 +33,7 @@ class Screen {
     let ht = (height / 15) * inc; //height of all
     noStroke();
     push();
-    translate(mybox.x - width / 2 + sWidth / 2 /*mybox.x - (sWidth + (nWidth * 4)) / 2*/ , -280, 0);
+    translate(mybox.x - width / 2 + sWidth / 2 /*mybox.x - (sWidth + (nWidth * 4)) / 2*/ , -height/2 + ht/2, 0);
     texture(getImage(3));
     plane(sWidth, ht); //SCORE
     translate((sWidth + nWidth) / 2 + (num * nWidth), 0, 0);
@@ -48,7 +48,7 @@ class Screen {
     if (lvl < 11) {
       let lWidth = (width * 11 / 75) * inc; //"Level" width
       push();
-      translate(mybox.x + width / 2 - (lWidth / 2 + nWidth * 2), -280, 0);
+      translate(mybox.x + width / 2 - (lWidth / 2 + nWidth * 2),  -height/2 + ht/2, 0);
       texture(getImage(6));
       plane(lWidth, ht); //LEVEL
       translate(lWidth / 2 + nWidth * 5 / 2, 0, 0);
@@ -63,21 +63,21 @@ class Screen {
     } else if (lvl === 11) {
       let lWidth = (width * 17 / 75) * inc; //"Level" width
       push();
-      translate(mybox.x + width / 2 - lWidth / 2, -280, 0);
+      translate(mybox.x + width / 2 - lWidth / 2, -height/2 + ht/2, 0);
       texture(getImage(7));
       plane(lWidth, ht); //FINAL LEVEL
       pop();
     }
   }
   levelup(lvl) {
-    this.z = this.z + 2; //speed of movement towards the screen
+    this.z = this.z + 3; //speed of movement towards the screen
     let inc = 3;
     let ht = (height / 15) * inc; //height of both "Level" and number
     if (lvl < 11) {
       let lWidth = (width * 11 / 75) * inc; //"Level" width
       let nWidth = (width * 3 / 100) * inc; //number width
       push();
-      translate(mybox.x - (lWidth + (2 * nWidth)) / 2 + lWidth / 2, -170, this.z);
+      translate(mybox.x - (lWidth + (2 * nWidth)) / 2 + lWidth / 2, -height/4, this.z);
       texture(getImage(6));
       plane(lWidth, ht); //LEVEL
       translate(lWidth / 2 + nWidth * 5 / 2, 0, 0);
@@ -92,13 +92,13 @@ class Screen {
     } else if (lvl === 11) {
       let lWidth = (width * 17 / 75) * inc; //"Level" width
       push();
-      translate(mybox.x, -170, this.z);
+      translate(mybox.x, -height/4, this.z);
       texture(getImage(7));
       plane(lWidth, ht); //FINAL LEVEL
       pop();
     } else if (lvl === 12) {
       push();
-      translate(mybox.x, -170, 0);
+      translate(mybox.x, -height/4, 0);
       texture(getImage(8));
       plane((width*1/4)*inc , (height/15)*inc); //GAME OVER
       pop();
@@ -113,11 +113,10 @@ class Screen {
     };
     push();
     noStroke();
-    background(169, 209, 142);
-    translate(0, 0, 490);
+    translate(0, 0, width*49/60);
     this.format(getImage(2), getBase().w, getBase().h / 3);//Game Over Screen
     score=int(score);
-    translate(((score + "").length) * num.w / 2 + num.w / 2, -15, 0);
+    translate(((score + "").length) * num.w / 2 + num.w / 2, -height/40, 0);
     while (score != 0) {
       translate(-num.w, 0, 0);
       texture(getImage(4)[score % 10]);
@@ -129,11 +128,9 @@ class Screen {
   format(graphics, w, h) {
     //cam.setPosition(0, 0, base.h * 75 / 90);
     push();
-    //rotateX(-80);
     noStroke();
     texture(graphics);
     plane(w, h);
-    //rotateX(80);
     pop();
   }
   reset() {
@@ -141,7 +138,7 @@ class Screen {
   }
 
   arrow() {
-    let inc=0.1;
+    let inc=getSize(true)/300;
     strokeWeight(0.1);
     stroke(0,190,82);
     fill(0,128,55);
