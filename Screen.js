3b73b3b146eedc7294393f8722b0f1d15accc45f
Azhar Ahmed <49714602+ahmedazhar05@users.noreply.github.com>
2019-06-08 21:20:57 +0530

    Update Screen.js

--- Screen.js
+++ Screen.js
@@ -4,40 +4,39 @@ class Screen {
   }
   start() {
     push();
-    translate(0, 0, width*49/60);
+    translate(0, 0, width * 49 / 60);
     this.format(getImage(1), getBase().w, getBase().h / 3);
     pop();
   }
   enter() {
     let inc = 3;
     push();
-    translate(mybox.x, /*-180*/-height/2 + (height/15)*inc, /*mybox.y-400*/0);
+    translate(0, -height / 2 + (height / 15) * inc, 0);
     noStroke();
     texture(getImage(5));
-    plane((width/10)*inc, (height/15)*inc);
-    //translate(mybox.x, mybox.z + getSize(true)/2, mybox.y);
-    translate(0, +height/2 - (height/15)*inc + mybox.z + getSize(true)/2/*mybox.z + getSize(true)/2 +180*/, mybox.y);
-    rotateX(millis()/10);
-    translate(getSize(true)*3/2, 0,0);
+    plane((width / 10) * inc, (height / 15) * inc);
+    translate(mybox.x, +height / 2 - (height / 15) * inc + mybox.z + getSize(true) / 2, mybox.y);
+    rotateX(millis() / 10);
+    translate(getSize(true) * 3 / 2, 0, 0);
     this.arrow();
-    translate(-getSize(true)*3, 0, 0);
+    translate(-getSize(true) * 3, 0, 0);
     rotateZ(-180);
     this.arrow();
     pop();
   }
   score(sc, lvl) { //game score update on top
-    let num = 5; //max digits of score
-    let inc = 1.5; //digit size increment
-    let nWidth = (width * 3 / 100) * inc; //number width
-    let sWidth = (width * 3 / 20) * inc; //"Score" width
-    let ht = (height / 15) * inc; //height of all
+    let num = 5, //max digits of score
+      inc = 1.5, //digit size increment
+      nWidth = (width * 3 / 100) * inc,//number width
+      sWidth = (width * 3 / 20) * inc, //"Score" width
+      ht = (height / 15) * inc; //height of all
     noStroke();
     push();
-    translate(mybox.x - width / 2 + sWidth / 2 /*mybox.x - (sWidth + (nWidth * 4)) / 2*/ , -height/2 + ht/2, 0);
+    translate(mybox.x - width / 2 + sWidth / 2, -height / 2 + ht / 2, 0);
     texture(getImage(3));
     plane(sWidth, ht); //SCORE
     translate((sWidth + nWidth) / 2 + (num * nWidth), 0, 0);
-    sc = int(sc)*10;
+    sc = int(sc) * 10;
     for (let i = 0; i < num; i++) {
       sc = int(sc / 10);
       translate(-nWidth, 0, 0);
@@ -48,7 +47,7 @@ class Screen {
     if (lvl < 11) {
       let lWidth = (width * 11 / 75) * inc; //"Level" width
       push();
-      translate(mybox.x + width / 2 - (lWidth / 2 + nWidth * 2),  -height/2 + ht/2, 0);
+      translate(mybox.x + width / 2 - (lWidth / 2 + nWidth * 2), -height / 2 + ht / 2, 0);
       texture(getImage(6));
       plane(lWidth, ht); //LEVEL
       translate(lWidth / 2 + nWidth * 5 / 2, 0, 0);
@@ -63,7 +62,7 @@ class Screen {
     } else if (lvl === 11) {
       let lWidth = (width * 17 / 75) * inc; //"Level" width
       push();
-      translate(mybox.x + width / 2 - lWidth / 2, -height/2 + ht/2, 0);
+      translate(mybox.x + width / 2 - lWidth / 2, -height / 2 + ht / 2, 0);
       texture(getImage(7));
       plane(lWidth, ht); //FINAL LEVEL
       pop();
@@ -71,13 +70,13 @@ class Screen {
   }
   levelup(lvl) {
     this.z = this.z + 3; //speed of movement towards the screen
-    let inc = 3;
-    let ht = (height / 15) * inc; //height of both "Level" and number
+    let inc = 3,
+        ht = (height / 15) * inc; //height of both "Level" and number
     if (lvl < 11) {
-      let lWidth = (width * 11 / 75) * inc; //"Level" width
-      let nWidth = (width * 3 / 100) * inc; //number width
+      let lWidth = (width * 11 / 75) * inc, //"Level" width
+          nWidth = (width * 3 / 100) * inc; //number width
       push();
-      translate(mybox.x - (lWidth + (2 * nWidth)) / 2 + lWidth / 2, -height/4, this.z);
+      translate(mybox.x - (lWidth + (2 * nWidth)) / 2 + lWidth / 2, -height / 4, this.z);
       texture(getImage(6));
       plane(lWidth, ht); //LEVEL
       translate(lWidth / 2 + nWidth * 5 / 2, 0, 0);
@@ -92,31 +91,39 @@ class Screen {
     } else if (lvl === 11) {
       let lWidth = (width * 17 / 75) * inc; //"Level" width
       push();
-      translate(mybox.x, -height/4, this.z);
+      translate(mybox.x, -height / 4, this.z);
       texture(getImage(7));
       plane(lWidth, ht); //FINAL LEVEL
       pop();
     } else if (lvl === 12) {
       push();
-      translate(mybox.x, -height/4, 0);
+      translate(mybox.x, -height / 4, 0);
       texture(getImage(8));
-      plane((width*1/4)*inc , (height/15)*inc); //GAME OVER
+      plane((width * 1 / 4) * inc, (height / 15) * inc); //GAME OVER
       pop();
     }
-
   }
-  over(score) { //game score
-    let inc = 1.25; //size incremental value
-    let num = {
-      w: (width * 3 / 100) * inc,
-      h: (height / 15) * inc,
-    };
+  over(score, b) { //game score
+    background(219, 219, 219);
+    let i, h;
+    if (b) {
+      i = 2;
+      h = -height / 40;
+    } else {
+      i = 9;
+      h = height / 7.5;
+    }
+    let inc = 1.25, //size incremental value
+      num = {
+        w: (width * 3 / 100) * inc,
+        h: (height / 15) * inc,
+      };
     push();
     noStroke();
-    translate(0, 0, width*49/60);
-    this.format(getImage(2), getBase().w, getBase().h / 3);//Game Over Screen
-    score=int(score);
-    translate(((score + "").length) * num.w / 2 + num.w / 2, -height/40, 0);
+    translate(0, 0, width * 49 / 60);
+    this.format(getImage(i), getBase().w, getBase().h / 3);//Game Over Screen
+    score = int(score);
+    translate(((score + "").length) * num.w / 2 + num.w / 2, h, 0);
     while (score != 0) {
       translate(-num.w, 0, 0);
       texture(getImage(4)[score % 10]);
@@ -126,7 +133,6 @@ class Screen {
     pop();
   }
   format(graphics, w, h) {
-    //cam.setPosition(0, 0, base.h * 75 / 90);
     push();
     noStroke();
     texture(graphics);
@@ -136,69 +142,68 @@ class Screen {
   reset() {
     this.z = 0;
   }
-
   arrow() {
-    let inc=getSize(true)/300;
+    let inc = getSize(true) / 300;
     strokeWeight(0.1);
-    stroke(0,190,82);
-    fill(0,128,55);
+    stroke(0, 190, 82);
+    fill(0, 128, 55);
     beginShape(TRIANGLE_STRIP);
     //backarrow full (square + triangle)
     vertex(-150 * inc, 75 * inc, -50 * inc);
     vertex(-150 * inc, -75 * inc, -50 * inc);
-    vertex(0 * inc, 75 * inc, -50 * inc);
-    vertex(0 * inc, -75 * inc, -50 * inc);
-    vertex(0 * inc, -150 * inc, -50 * inc);
+    vertex(0, 75 * inc, -50 * inc);
+    vertex(0, -75 * inc, -50 * inc);
+    vertex(0, -150 * inc, -50 * inc);
     vertex(150 * inc, 0 * inc, -50 * inc);
-    vertex(0 * inc, -150 * inc, -50 * inc);
-    vertex(0 * inc, 75 * inc, -50 * inc);
-    vertex(150 * inc, 0 * inc, -50 * inc);
-    vertex(0 * inc, 150 * inc, -50 * inc);
-    vertex(0 * inc, 75 * inc, -50 * inc);
+    vertex(0, -150 * inc, -50 * inc);
+    vertex(0, 75 * inc, -50 * inc);
+    vertex(150 * inc, 0, -50 * inc);
+    vertex(0, 150 * inc, -50 * inc);
+    vertex(0, 75 * inc, -50 * inc);
 
     //front arrow square
-    vertex(0 * inc, 75 * inc, 50 * inc);
+    vertex(0, 75 * inc, 50 * inc);
     vertex(-150 * inc, 75 * inc, 50 * inc);
     vertex(-150 * inc, -75 * inc, 50 * inc);
     vertex(-150 * inc, 75 * inc, -50 * inc);
     vertex(-150 * inc, -75 * inc, -50 * inc);
-    vertex(0 * inc, -75 * inc, -50 * inc);
+    vertex(0, -75 * inc, -50 * inc);
     vertex(-150 * inc, -75 * inc, 50 * inc);
-    vertex(0 * inc, -75 * inc, 50 * inc);
-    vertex(0 * inc, 75 * inc, 50 * inc);
+    vertex(0, -75 * inc, 50 * inc);
+    vertex(0, 75 * inc, 50 * inc);
 
     //arrow front triangle
-    vertex(0 * inc, -150 * inc, 50 * inc);
-    vertex(150 * inc, 0 * inc, 50 * inc);
-    vertex(0 * inc, -150 * inc, 50 * inc);
-    vertex(0 * inc, 75 * inc, 50 * inc);
-    vertex(150 * inc, 0 * inc, 50 * inc);
-    vertex(0 * inc, 150 * inc, 50 * inc);
-    vertex(0 * inc, 75 * inc, 50 * inc);
-    vertex(0 * inc, 150 * inc, -50 * inc);
+    vertex(0, -150 * inc, 50 * inc);
+    vertex(150 * inc, 0, 50 * inc);
+    vertex(0, -150 * inc, 50 * inc);
+    vertex(0, 75 * inc, 50 * inc);
+    vertex(150 * inc, 0, 50 * inc);
+    vertex(0, 150 * inc, 50 * inc);
+    vertex(0, 75 * inc, 50 * inc);
+    vertex(0, 150 * inc, -50 * inc);
 
     //arrow slanting botton half
-    vertex(150 * inc, 0 * inc, 50 * inc);
-    vertex(150 * inc, 0 * inc, -50 * inc);
+    vertex(150 * inc, 0, 50 * inc);
+    vertex(150 * inc, 0, -50 * inc);
 
     //arrow slanting top
-    vertex(0 * inc, -150 * inc, 50 * inc);
-    vertex(0 * inc, -150 * inc, -50 * inc);
+    vertex(0, -150 * inc, 50 * inc);
+    vertex(0, -150 * inc, -50 * inc);
 
     //arrow vertical top
-    vertex(0 * inc, -75 * inc, 50 * inc);
-    vertex(0 * inc, -75 * inc, -50 * inc);
+    vertex(0, -75 * inc, 50 * inc);
+    vertex(0, -75 * inc, -50 * inc);
 
     //arrow horizontal base
-    vertex(0 * inc, 75 * inc, -50 * inc);
+    vertex(0, 75 * inc, -50 * inc);
     vertex(-150 * inc, 75 * inc, 50 * inc);
     vertex(-150 * inc, 75 * inc, -50 * inc);
 
     //arrow slanting botton other half
-    vertex(0 * inc, 75 * inc, -50 * inc);
-    vertex(150 * inc, 0 * inc, 50 * inc);
-    vertex(0 * inc, 150 * inc, -50 * inc);
-    vertex(0 * inc, 150 * inc, 50 * inc);
+    vertex(0, 75 * inc, -50 * inc);
+    vertex(150 * inc, 0, 50 * inc);
+    vertex(0, 150 * inc, -50 * inc);
+    vertex(0, 150 * inc, 50 * inc);
     endShape();
   }
 }
