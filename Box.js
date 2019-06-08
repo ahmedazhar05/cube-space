661f58d537e0716da6a1951ccebd59c5674a453d
Azhar Ahmed <49714602+ahmedazhar05@users.noreply.github.com>
2019-06-08 21:20:20 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -1,29 +1,22 @@
-let size;
-let x, y, z;
+let size, x, y, z;
 class Box {
-
   constructor(bool) {
-    
-    let base=getBase();
-    
-    let colour;
+    let base = getBase(),
+      colour;
     if (bool) {
-
-      this.size = base.w/10; //30
+      this.size = base.w / 10;
       this.colour = [128, 0, 0];
       this.x = 0;
       this.y = (base.h - this.size) / 2 - base.h / 45; //cornering;
-
     } else { //hurdles
-
-      this.size = base.w*35/300; //35
-      this.colour = [50, 50, 50];
+      this.size = base.w * 35 / 300;
+      this.colour = [20, 20, 20];
       this.x = int(random(-base.w / 2 + this.size, base.w / 2 - this.size + 1)); //positioning
       this.y = -(base.h - this.size) / 2; //motion
-
+      
     }
-
     this.z = (base.d + this.size) / 2; //elevation;
+    
   }
   createBox(sp) {
     this.x = this.x + sp; //motion
@@ -37,18 +30,16 @@ class Box {
   }
   createHurdle(sp) {
     this.y = this.y + abs(sp); //motion
-
     push();
     translate(this.x, this.y, this.z);
     ambientLight(this.colour);
-    ambientMaterial(this.colour);
-    stroke(15);
+    stroke(0);
     strokeWeight(0.5);
     box(this.size);
     pop();
   }
-  
 }
-function getSize(b){
+
+function getSize(b) {
   return ((new Box(b)).size);
 }
